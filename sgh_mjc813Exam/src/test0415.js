5ef30f47f89e1ac5e167433bdfd9e84665e8c878
qhrmf34 <qhrmf34@naver.com>
2025-04-15 20:05:44 +0900

    4월 15일 문제풀이 수정

--- sgh_mjc813Exam/src/test0415.js
+++ sgh_mjc813Exam/src/test0415.js
@@ -1,25 +1,76 @@
-//로또
-let lotto= [];
-for(let k=0; k<6;k++){
-    let num = Math.ceil(Math.random()*45);
-    if(num!=0&&!lotto.includes(num)){
-        lotto.push(num);
+class Test0415{
+    exam1(){
+        let lotto= [];
+        for(let k=0; k<6;k++){
+            let num = Math.ceil(Math.random()*45);
+            if(num!=0&&!lotto.includes(num)){
+                lotto.push(num);
+            }
+            else{
+            k--;}
+        }
+        lotto.sort((a,b)=>{return a-b;});
+        document.getElementById("result1").innerText=`답 : ${JSON.stringify(lotto)}`;
+
     }
-    else{
-    k--;}
+    exam2(){
+    let a=Number(prompt("트리 입력하세요."));
+    const b=(a)=>{
+    let k="";
+    for(let i=1;i<=a;i++){
+            let gong="&nbsp;";
+            let star="*";
+            k+=`${gong.repeat(a-i)}${star.repeat(2*i-1)}<br>`;
+
+        }
+    document.getElementById("result2").innerHTML=k;
+
+    };
+    b(a);
+    }
+    exam3(){
+    let strInput=Number(prompt("행성 입력하세요."));
+    let a="";
+    switch(strInput){
+        case 1 : a="수성";
+        break;
+        case 2 : a="금성";
+        break;
+        case 3 : a="지구";
+        break;
+        case 4 : a="화성";
+        break;
+        case 5 : a="목성";
+        break;
+        case 6 : a="토성";
+        break;
+        case 7 : a="천왕성";
+        break;
+        case 8 : a="해왕성";
+        break;
+    }
+    document.getElementById("result4").innerHTML=`답 : ${a}`;
+
+    }
+    exam4(){
+    let strInput2=prompt("단어 입력하세요.");
+    const str2=(str)=>{
+        let bb="";
+        for(let a=str.length-1;a>=0;a--){
+            bb+=str.charAt(a);
+        }
+        return bb;
+    }
+    document.getElementById("result5").innerHTML=`답 : ${str2(strInput2)}`;
+
+    }
 }
-lotto.sort((a,b)=>{return a-b;});
-console.log(JSON.stringify(lotto));
-//트리
-let a=Number(prompt("트리 입력하세요."));
-const b=(a)=>{
-for(let i=1;i<=a;i++){
-        let gong=" ";
-        let star="*";
-        console.log(`${gong.repeat(a-i)}${star.repeat(2*i-1)}`);
-    }
-};
-b(a);
+let test0415=new Test0415();
+test0415.exam1();
+test0415.exam2();
+test0415.exam3();
+test0415.exam4();
+
 //출력
 class Wizard{
 constructor(health,mana,armor){
@@ -28,42 +79,10 @@ this.mana=mana;
 this.armor=armor;
 }
 attack(){
-console.log("파이어볼");
+document.getElementById("result3").innerHTML=`능력치 : ${this.health},${this.mana},${this.armor}<br> 기술 : 파이어볼`;
 }
 }
-const x=new Wizard(545, 210, 10);
-console.log(x.health, x.mana, x.armor);
-x.attack();
+let wizard=new Wizard(545,210,10);
+wizard.attack();
 
 
-//행성
-
-let strInput=Number(prompt("행성 입력하세요."));
-switch(strInput){
-    case 1 : console.log("수성");
-    break;
-    case 2 : console.log("금성");
-    break;
-    case 3 : console.log("지구");
-    break;
-    case 4 : console.log("화성");
-    break;
-    case 5 : console.log("목성");
-    break;
-    case 6 : console.log("토성");
-    break;
-    case 7 : console.log("천왕성");
-    break;
-    case 8 : console.log("해왕성");
-    break;
-}
-//거꾸로
-let strInput2=prompt("단어 입력하세요.");
-const str2=(str)=>{
-    let bb="";
-    for(let a=str.length-1;a>=0;a--){
-        bb+=str.charAt(a);
-    }
-    return bb;
-}
-console.log(str2(strInput2));
