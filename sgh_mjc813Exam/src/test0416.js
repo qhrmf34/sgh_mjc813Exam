f61b601db3b91967f73f4cdf0911f7049a20ea89
qhrmf34 <qhrmf34@naver.com>
2025-04-16 21:21:55 +0900

    25. 26번

--- sgh_mjc813Exam/src/test0416.js
+++ sgh_mjc813Exam/src/test0416.js
@@ -39,15 +39,17 @@ class Test0416{
     //키
     exam17(){
 //        let a= Number(prompt("키를 입력하세요."));
-        let a = document.getElementById("exam17").value;
-        a = Number(a);
-        if(a>=150){
-            document.getElementById("result17").innerHTML=`키 : ${a} <br> YES`;
-    }
-        else{
-            document.getElementById("result17").innerHTML=`키 : ${a} <br> NO`;
 
-    }
+            let a = document.getElementById("exam17").value;
+            a = Number(a);
+
+            if(a>=150){
+                document.getElementById("result17").innerHTML=`키 : ${a} <br> YES`;
+        }
+            else{
+                document.getElementById("result17").innerHTML=`키 : ${a} <br> NO`;
+
+        }
 
     }
     //성적 평균
@@ -60,34 +62,75 @@ class Test0416{
         c = Number(c);
         document.getElementById("result18").innerHTML=`입력 : ${a} ${b} ${c} <br> 출력 : ${Math.round((a+b+c)/3)}`;
     }
-    //제곱
-    exam19(){
-        let a=document.getElementById("exam190").value;
-        let b=document.getElementById("exam191").value;
-        a = Number(a);
-        b = Number(b);
-        document.getElementById("result19").innerHTML=`결과 : ${a**b}`;
-    }
+
     exam20(){
            let a=document.getElementById("exam200").value;
            let b=document.getElementById("exam201").value;
            a = Number(a);
            b = Number(b);
-           document.getElementById("result20").innerHTML=`입력 : ${a} ${b}<br> 출력 : ${a/b} ${a%b}`;
+           document.getElementById("result20").innerHTML=`입력 : ${a} ${b}<br> 출력 : ${Math.round(a/b)} ${a%b}`;
        }
     exam24(){
              let a=document.getElementById("exam24").value;
              document.getElementById("result24").innerHTML=`입력 : ${a} <br> 출력 : ${a.toUpperCase()} `;
          }
 
-}
+
+    }
+
 //상속 테스트
 class Renewal extends Test0416{
     show24(){
         super.exam24();
     }
 }
+class Exam19{
+  //제곱
+    static exam19(){
+        let a=document.getElementById("exam190").value;
+        let b=document.getElementById("exam191").value;
+        a = Number(a);
+        b = Number(b);
+        document.getElementById("result19").innerHTML=`결과 : ${a**b}`;
+    }
+}
+class Exam25{
+  //제곱
+    static exam25(){
+        let a=document.getElementById("exam25").value;
+        a = Number(a);
+        document.getElementById("result25").innerHTML=`결과 : ${this.circle(a)}`;
+    }
+    static circle(b){
+        return b*b*Math.PI;
+    }
 
+}
+
+class Exam26{
+    static exam26(){
+             let a=document.getElementById("exam26").value;
+             switch(a){
+             case "수성" : document.getElementById("result26").innerHTML=`Mercury`;
+                break;
+             case "금성" : document.getElementById("result26").innerHTML=`Venus`;
+                break;
+             case "지구" : document.getElementById("result26").innerHTML=`Earth`;
+                break;
+             case "화성" : document.getElementById("result26").innerHTML=`Mars`;
+                break;
+             case "목성" : document.getElementById("result26").innerHTML=`Jupiter`;
+                break;
+             case "토성" : document.getElementById("result26").innerHTML=`Saturn`;
+                 break;
+             case "천왕성" : document.getElementById("result26").innerHTML=`Uranus`;
+                 break;
+             case "해왕성" : document.getElementById("result26").innerHTML=`Neptune`;
+                 break;
+             default : document.getElementById("result26").innerHTML=`다시 입력해주세요.`;
+             }
+    }
+}
 function start() {
     let test0416 = new Test0416();
     let renewal = new Renewal();
@@ -97,8 +140,9 @@ function start() {
     test0416.exam15();
     test0416.exam17();
     test0416.exam18();
-    test0416.exam19();
+    Exam19.exam19();
     test0416.exam20();
     renewal.show24();
-
+    Exam25.exam25();
+    Exam26.exam26();
 }
