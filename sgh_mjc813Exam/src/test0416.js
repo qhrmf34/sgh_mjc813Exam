1d8e702fa1730d6aa7050f48e722259823ac4009
qhrmf34 <qhrmf34@naver.com>
2025-04-16 17:53:18 +0900

    4월 16일 문제풀이 순서 수정.11번 추가.html 변경

--- sgh_mjc813Exam/src/test0416.js
+++ sgh_mjc813Exam/src/test0416.js
@@ -1,47 +1,71 @@
 class Test0416{
-    exam0(){
+    //입력한 숫자까지 모든 수 더하기
+     exam11(){
+           let a=document.getElementById("exam11").value;
+           a=Number(a);
+           let b=[];
+           for(let i=1;i<=a;i++){
+                b.push(i);
+           }
+           let d=b.reduce((total,value)=>{return total+value;});
+           document.getElementById("result0").innerHTML=`입력 : ${a}<br> total = ${d}`;
+
+        }
+     //369
+     exam14(){
+         let a=document.getElementById("exam14").value;
+         a = Number(a);
+         if(a%3==0){
+         document.getElementById("result1").innerHTML=`입력 : ${a}<br>출력 : 짝`;
+         }
+         else{
+         document.getElementById("result1").innerHTML=`입력 : ${a}<br>출력 : ${a}`;
+         }
+     }
+    //이름 입력
+    exam15(){
+       let a=document.getElementById("exam15").value;
+       document.getElementById("result2").innerHTML=`입력 : ${a}<br>안녕하세요. 저는 ${a}입니다.`;
+
+    }
+
+    //키
+    exam17(){
 //        let a= Number(prompt("키를 입력하세요."));
         let a = document.getElementById("exam17").value;
         a = Number(a);
         if(a>=150){
-            document.getElementById("result0").innerHTML=`키 : ${a} <br> YES`;
+            document.getElementById("result3").innerHTML=`키 : ${a} <br> YES`;
     }
         else{
-            document.getElementById("result0").innerHTML=`키 : ${a} <br> NO`;
+            document.getElementById("result3").innerHTML=`키 : ${a} <br> NO`;
 
     }
 
     }
-    exam1(){
+    //성적 평균
+    exam18(){
         let a=document.getElementById("exam180").value;
         let b=document.getElementById("exam181").value;
         let c=document.getElementById("exam182").value;
         a = Number(a);
         b = Number(b);
         c = Number(c);
-        document.getElementById("result1").innerHTML=`입력 : ${a} ${b} ${c} <br> 출력 : ${Math.round((a+b+c)/3)}`;
+        document.getElementById("result4").innerHTML=`입력 : ${a} ${b} ${c} <br> 출력 : ${Math.round((a+b+c)/3)}`;
     }
-    exam2(){
-        let a=document.getElementById("exam14").value;
-        a = Number(a);
-        if(a%3==0){
-        document.getElementById("result2").innerHTML=`입력 : ${a}<br>출력 : 짝`;
-        }
-        else{
-        document.getElementById("result2").innerHTML=`입력 : ${a}<br>출력 : ${a}`;
-        }
-    }
-    exam3(){
-       let a=document.getElementById("exam15").value;
-       document.getElementById("result3").innerHTML=`입력 : ${a}<br>안녕하세요. 저는 ${a}입니다.`;
 
-    }
+
+
+
+
 }
 
 function start() {
     let test0416 = new Test0416();
-    test0416.exam0();
-    test0416.exam1();
-    test0416.exam2();
-    test0416.exam3();
+    test0416.exam11();
+    test0416.exam14();
+    test0416.exam15();
+    test0416.exam17();
+    test0416.exam18();
+
 }
