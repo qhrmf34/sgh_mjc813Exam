2d55d01c0fbec40200aac68376632a93e2c3af94
qhrmf34 <qhrmf34@naver.com>
2025-04-17 20:13:02 +0900

    37번

--- sgh_mjc813Exam/src/test0417.js
+++ sgh_mjc813Exam/src/test0417.js
@@ -37,10 +37,9 @@ class Exam28{
     static exam28(){
         let a=document.getElementById("exam28").value;
         let b="";
-        let c="";
         for(let i=0; i<a.length;i++){
             if(i+1!=a.length){
-            b+=`${a.charAt(i)} ${a.charAt(i+1)}<br>`;
+                b+=`${a.charAt(i)} ${a.charAt(i+1)}<br>`;
             }
         }
 
@@ -55,4 +54,40 @@ document.getElementById("button28").onclick = ()=>{
 };
 
 
+class Exam37{
+    arrItem=[];
+    maps = new Map();
+    constructor(input){
+        this.arrItem=input.split(" ");
+    }
+    checkVotes(){
+    for(let a of this.arrItem){
+        if(this.maps.has(a)){
+            this.maps.set(a,this.maps.get(a)+1);
+        }
+        else{
+            this.maps.set(a,1);
+        }
+    }
+    let h="";
+    let g=Math.max(...this.maps.values());
+        this.maps.forEach((value,key)=>{
+            if(value==g){
+                h=key;
+            }
+        });
+        document.getElementById("result37").innerHTML=`${h}(이)가 총 ${g}표로 반장이 되었습니다.`;
+        //배열의 원소를 검색하여 같은 원소가 나오면 몇번인지 저장
+        //map 에다가 원소이름 : 몇번으로 저장한다.
+        //다음 원소로 검색할때 map의 키에 원소이름이 존재하는지 조건
+        //if(this.maps.has(원소이름)==true)
+        //원소 이름이 이미 map에 있다면 다음 배열 원소로 건너뛴다.
+        //가장 값이 큰 map 원소를 리턴
 
+    }
+}
+
+document.getElementById("button37").onclick = ()=>{
+    let ex37=new Exam37(document.getElementById("exam37").value);
+    ex37.checkVotes();
+};
