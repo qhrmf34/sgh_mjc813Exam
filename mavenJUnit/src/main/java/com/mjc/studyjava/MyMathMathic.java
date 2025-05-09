50666c178ebb5687930fd28f7b1e9a2604ebb10d
qhrmf34 <qhrmf34@naver.com>
2025-05-09 17:30:28 +0900

    JUnit 라이브러리 실행

---
+++ mavenJUnit/src/main/java/com/mjc/studyjava/MyMathMathic.java
@@ -0,0 +1,66 @@
+package com.mjc.studyjava;
+
+public class MyMathMathic {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public long mul(int a, int b){
+        return (long)a*b;
+    }
+    public int divM(int a, int b){
+        return a/b;
+    }
+    public int divN(int a, int b){
+        return a%b;
+    }
+    public boolean same(int a,int b){
+        return a==b;
+    }
+    public boolean same(String a, String b){
+        return a.equals(b);
+    }
+    public boolean notSame(int a, int b){
+        return !(a==b);
+    }
+    public boolean big(int a,int b){
+        return a>b;
+    }
+    public boolean small(int a,int b){
+        return a<b;
+    }
+    public int shiftLeft(int a,byte b){
+        return a<<b;
+    }
+    public int shiftRight(int a, byte b){
+        return a>>b;
+    }
+    public int shiftRight0(int a, byte b){
+        return a>>>b;
+    }
+    public long square(int a, byte b){
+        long answer=1;
+        for(int i=0; i<b; i++){
+            answer *= (long)a;
+        }
+        return answer;
+    }
+    public int absolute(int a){
+        return Math.abs(a);
+    }
+    public String toHex(int a){
+        return Integer.toHexString(a);
+    }
+    public String toBin(int a){
+        return Integer.toBinaryString(a);
+    }
+    public String toOct(int a){
+        return Integer.toOctalString(a);
+    }
+    public int toDec(String a){
+        return Integer.parseInt(a);
+    }
+}
