0d5d06f60e62011ec9d18f745b8556fbc3da6568
qhrmf34 <qhrmf34@naver.com>
2025-05-09 20:58:49 +0900

    연습문제 6번

---
+++ sgh_mjc813Exam/src/com/mjc/mjc813/cal/question4.java
@@ -0,0 +1,10 @@
+package com.mjc.mjc813.cal;
+
+public class question4 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            System.out.println("*".repeat(i));
+        }
+
+    }
+}
