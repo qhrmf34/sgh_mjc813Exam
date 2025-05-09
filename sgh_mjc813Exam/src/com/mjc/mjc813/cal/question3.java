ca7eb8fe2eba07e7daa88507629f96437c498da2
qhrmf34 <qhrmf34@naver.com>
2025-05-09 20:55:39 +0900

    연습문제 5번 2차원 가변배열

---
+++ sgh_mjc813Exam/src/com/mjc/mjc813/cal/question3.java
@@ -0,0 +1,19 @@
+package com.mjc.mjc813.cal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class question3 {
+    public static void main(String[] args) {
+        List<int[]> sol = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 30; j++) {
+                if (4 * i + 5 * j == 60) {
+                    sol.add(new int[]{i, j});
+                }
+            }
+        }
+        int[][] answer = sol.toArray(new int[0][]);
+        System.out.println(Arrays.deepToString(answer));
+    }
+    }
+
