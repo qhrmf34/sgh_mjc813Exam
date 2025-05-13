a751f9145d23ab1125ac0029acdcc4d308615714
qhrmf34 <qhrmf34@naver.com>
2025-05-13 20:49:39 +0900

    프로그래머스 181947

---
+++ sgh_mjc813Exam/src/test/programmers/Solution27.java
@@ -0,0 +1,12 @@
+package test.programmers;
+import java.util.Scanner;
+
+public class Solution27 {
+    //school.programmers.co.kr/learn/courses/30/lessons/181947
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a +" + "+ b +" = "+(a+b));
+    }
+}
