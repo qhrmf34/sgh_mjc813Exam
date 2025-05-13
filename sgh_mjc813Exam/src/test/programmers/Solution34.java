f5e3d5af48e044226ca103fc5b329f63d54dc1c0
qhrmf34 <qhrmf34@naver.com>
2025-05-13 21:42:05 +0900

    프로그래머스 181850

---
+++ sgh_mjc813Exam/src/test/programmers/Solution34.java
@@ -0,0 +1,13 @@
+package test.programmers;
+
+public class Solution34 {
+    //school.programmers.co.kr/learn/courses/30/lessons/181850
+    public static void main(String[] args) {
+        Solution34 sol = new Solution34();
+        System.out.println(sol.solution(1.42));
+        System.out.println(sol.solution(69.32));
+    }
+    public int solution(double flo) {
+        return (int)flo;
+    }
+}
