2d0e2dfc4cb84415aef5d28c56337a5e675d6ebd
qhrmf34 <qhrmf34@naver.com>
2025-05-13 20:46:05 +0900

    프로그래머스 181945

---
+++ sgh_mjc813Exam/src/test/programmers/Solution26.java
@@ -0,0 +1,13 @@
+package test.programmers;
+import java.util.Scanner;
+
+public class Solution26 {
+    //school.programmers.co.kr/learn/courses/30/lessons/181945
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i=0;i<a.length();i++){
+            System.out.println(a.charAt(i));
+        }
+    }
+}
