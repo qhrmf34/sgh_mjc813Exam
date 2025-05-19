108137575158a926173ecff3deff3f846c0ac8dc
qhrmf34 <qhrmf34@naver.com>
2025-05-19 19:59:39 +0900

    16번

---
+++ sgh_mjc813Exam/src/com/mjc/mjc813/cal/Printer.java
@@ -0,0 +1,16 @@
+package com.mjc.mjc813.cal;
+
+public class Printer {
+    public void println(int x) {
+        System.out.println(x);
+    }
+    public void println(String x) {
+        System.out.println(x);
+    }
+    public void println(boolean x) {
+        System.out.println(x);
+    }
+    public void println(double x) {
+        System.out.println(x);
+    }
+}
