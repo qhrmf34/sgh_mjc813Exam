f0e2800917f8f0fa48fb54cd96119ffcf445555d
qhrmf34 <qhrmf34@naver.com>
2025-05-19 19:59:12 +0900

    13,14번

---
+++ sgh_mjc813Exam/src/com/mjc/mjc813/cal/Member.java
@@ -0,0 +1,12 @@
+package com.mjc.mjc813.cal;
+
+public class Member {
+    String name;
+    String id;
+    String password;
+    int age;
+    Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
