d5a74240d6fd165b2d5bb7cdaf43888984a79889
qhrmf34 <qhrmf34@naver.com>
2025-06-10 15:53:20 +0900

    서버 수정

--- sgh_mjc813Exam/src/com/mjc/network/ServerApp.java
+++ sgh_mjc813Exam/src/com/mjc/network/ServerApp.java
@@ -5,9 +5,13 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ServerApp {
     private ServerSocket ss = null;
     private Scanner sc = null;
+    private ExecutorService es = Executors.newFixedThreadPool(10);
 
     public ServerApp(int port) throws IOException {
         // this.ss = new ServerSocket(port);   // 포트번호로 서버소켓 생성
@@ -15,10 +19,9 @@ public class ServerApp {
         this.ss.bind(new InetSocketAddress(44567)); // 바인드 명령으로 접속받아들일 IP주소대역과 포트번호로 클라이언트 접속을 대기할 수 있다.
     }
     public void startThread(Socket sck) {
-        Thread ss=new Thread(()->{
-            BufferedReader reader = null;
+        es.execute(()->{
             try {
-                reader = new BufferedReader(
+                BufferedReader reader = new BufferedReader(
                         new InputStreamReader(sck.getInputStream())
                 );
                 String str;
@@ -30,20 +33,20 @@ public class ServerApp {
                 throw new RuntimeException(e);
             }
         });
-
-        ss.start();
     }
     public void init() throws IOException {
-        String str;
-        try {
-            Socket sck = this.ss.accept();  // 클라이언트접속 기다림, 블로킹상태
-            System.out.println(sck.toString()); // 접속되면 이 문장 실행
+        while(true){
+            try {
+                Socket sck = this.ss.accept();  // 클라이언트접속 기다림, 블로킹상태
+                System.out.println(sck.toString()); // 접속되면 이 문장 실행
 
-            startThread(sck);
+                startThread(sck);
 
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            throw e;
+            }
+            catch (IOException e) {
+                System.err.println(e.getMessage());
+                throw e;
+            }
         }
     }
 
