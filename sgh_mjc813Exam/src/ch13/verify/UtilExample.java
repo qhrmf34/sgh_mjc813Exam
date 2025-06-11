d9650c4fc202021f6cef5d4c4bdf178df6ddc340
qhrmf34 <qhrmf34@naver.com>
2025-06-11 21:43:56 +0900

    오류 수정

--- sgh_mjc813Exam/src/ch13/verify/UtilExample.java
+++ sgh_mjc813Exam/src/ch13/verify/UtilExample.java
@@ -1,7 +1,5 @@
 package ch13.verify;
 
-import ch01.verify.Child;
-
 public class UtilExample {
     public static void main(String[] args) {
         Pair<String,Integer> pair = new Pair<String,Integer>("홍길동",35);
