c571ae9e8214085d62c5ba818f1fad1e636cc98d
qhrmf34 <qhrmf34@naver.com>
2025-06-17 15:27:00 +0900

    수정

--- sgh_mjc813Exam/src/com/mjc/lamda/INumber.java
+++ sgh_mjc813Exam/src/com/mjc/lamda/INumber.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class INumber {
+    private static List<Integer> test=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
     public static void main(String[] args) {
         Person person = new Person();
 
@@ -14,20 +16,13 @@ public class INumber {
             System.out.printf("자동차 %s의 년식은 %d 입니다.\n", name, age);
         });
         person.printArray((List<Integer> a)->{
-            Long sum=0L;
-            for(Integer i:a){
-                sum+=i;
-            }
+            Long sum=ssum(a);
             System.out.printf("합은 {%d} 입니다.\n", sum);
             return sum;
         });
         person.printArray((List<Integer> a)->{
-            Long sum=0L;
-            Long result=0L;
-            for(Integer i:a){
-                sum+=i;
-            }
-            result=sum/a.size();
+            Long sum=ssum(a);
+            Long result=sum/a.size();
             System.out.printf("평균은 {%d} 입니다.\n",result);
             return result;
         });
@@ -36,21 +31,24 @@ public class INumber {
         INumberProc aa = (name,age)->System.out.printf("자동차 %s의 년식은 %d 입니다.\n", name, age);
         aa.process("카니발",33);
         IArrayProc arraySum=((List<Integer> a)->{
-            Long sum = 0L;
-            for(Integer i:a) sum+=i;
+            Long sum=ssum(a);
             System.out.printf("합은 {%d} 입니다.\n", sum);
             return sum;
         });
-        List<Integer> test=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
         arraySum.process(test);
         IArrayProc arrayAvg=((List<Integer> a)->{
-            long sum=0l;
-            for(Integer i:a) sum+=i;
+            long sum=ssum(a);
             long result=(sum/a.size());
             return result;
         });
         System.out.printf("평균은 {%d} 입니다.\n",arrayAvg.process(test));
     }
 
-
+    public static Long ssum(List<Integer> a) {
+        Long sum=0L;
+        for(Integer i:a){
+            sum+=i;
+        }
+        return sum;
+    }
 }
