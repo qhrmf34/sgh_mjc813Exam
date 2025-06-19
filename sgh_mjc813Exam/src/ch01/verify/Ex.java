e6c1fcff78e1141b37045099258fe5711af6686c
qhrmf34 <qhrmf34@naver.com>
2025-06-19 15:31:44 +0900

    stream test

---
+++ sgh_mjc813Exam/src/ch01/verify/Ex.java
@@ -0,0 +1,22 @@
+package ch01.verify;
+
+public class Ex {
+    private Integer a;
+    private String b;
+    public Ex(Integer a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+    public Integer getA() {
+        return a;
+    }
+    public String getB() {
+        return b;
+    }
+    public void setA(Integer a) {
+        this.a = a;
+    }
+    public void setB(String b) {
+        this.b = b;
+    }
+}
--- sgh_mjc813Exam/src/ch01/verify/Example.java
+++ sgh_mjc813Exam/src/ch01/verify/Example.java
@@ -1,7 +1,20 @@
 package ch01.verify;
 
+import java.util.*;
+
 public class Example {
     public static void main(String[] args){
         System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+        List<Ex> exs = new ArrayList<Ex>();
+        exs.add(new Ex(null,"a"));
+        exs.add(new Ex(null,"b"));
+        exs.add(new Ex(null,"c"));
+
+        int d = exs.stream()
+                .filter(e -> e.getA() != null)
+                .mapToInt(e -> e.getA())
+                .max()
+                .orElse(4);
+        System.out.println(d);
     }
 }
